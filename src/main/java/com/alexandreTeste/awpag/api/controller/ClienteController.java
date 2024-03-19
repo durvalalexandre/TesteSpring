867566da3d7fe130905574883b2f7e14209d1074
durvalalexandre <durval.alexandre@gmail.com>
2024-03-19 15:47:13 -0300

    criar webAPP spring

---
+++ src/main/java/com/alexandreTeste/awpag/api/controller/ClienteController.java
@@ -0,0 +1,28 @@
+package com.alexandreTeste.awpag.api.controller;
+
+import com.alexandreTeste.awpag.domain.model.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ClienteController {
+    @GetMapping("/clientes")
+    public List<Cliente> listar(){
+        var cliente1 = new Cliente();
+        cliente1.setId(1L);
+        cliente1.setNome("joao b");
+        cliente1.setTelefone("11 93333 1111");
+        cliente1.setEmail("joao@gmail.com");
+
+        var cliente2 = new Cliente();
+        cliente2.setId(2L);
+        cliente2.setNome("maria");
+        cliente2.setTelefone("11 99999 4444");
+        cliente2.setEmail("maria@gmail.com");
+
+        return Arrays.asList(cliente1, cliente2);
+     }
+}
---
+++ src/main/java/com/alexandreTeste/awpag/domain/model/Cliente.java
@@ -0,0 +1,14 @@
+package com.alexandreTeste.awpag.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+    private long id;
+    private  String nome;
+    private String email;
+    private String telefone;
+
+}
